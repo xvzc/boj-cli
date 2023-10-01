c0c10461ec87f7341ea26ce77f1b6ad182ff5332
xvzc <dev.kwanghoo@gmail.com>
2023-10-01 17:23:14 +0900

    chore: Improve build process

--- setup.py
+++ setup.py
@@ -2,45 +2,9 @@ import os
 
 from setuptools import setup
 
-import version
-
-latest_version = version.get_latest_version("boj-cli")
 if "BOJ_CLI_NEXT_VERSION" in os.environ:
-    cur_version: str = os.environ["BOJ_CLI_NEXT_VERSION"][1:]
+    cur_version: str = os.environ["BOJ_CLI_NEXT_VERSION"].replace("v", "")
 else:
-    cur_version = str(latest_version)
+    cur_version = "0.0.0"
 
-setup(
-    long_description="Command line interface for BOJ",
-    entry_points={
-        "console_scripts": ["boj=boj.entry:cli"],
-    },
-    version=cur_version,
-    name="boj-cli",
-    author="xvzc",
-    url="https://github.com/xvzc/boj-cli",
-    install_requires=[
-        "beautifulsoup4==4.11.1",
-        "certifi==2022.12.7",
-        "cffi==1.15.1",
-        "charset-normalizer==3.0.1",
-        "cryptography==39.0.0",
-        "greenlet==2.0.1",
-        "idna==3.4",
-        "mdurl==0.1.2",
-        "selenium==4.11.2",
-        "msgpack==1.0.4",
-        "pycparser==2.21",
-        "chromedriver-autoinstaller==0.6.2",
-        "Pygments==2.14.0",
-        "requests==2.28.2",
-        "rich==13.2.0",
-        "six==1.16.0",
-        "soupsieve==2.3.2.post1",
-        "urllib3==1.26.14",
-        "websockets==10.4",
-        "PyYAML==6.0",
-        "dependency-injector==4.41.0",
-        "webdriver_manager==4.0.0",
-    ],
-)
+setup(version=cur_version)
