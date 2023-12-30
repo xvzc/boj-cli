3d109d7f342f063bf1f1d44677b907352e6846b6
xvzc <dev.kwanghoo@gmail.com>
2023-12-30 23:48:38 +0900

    fix(test): set absolute path for the fixtures

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -1,4 +1,6 @@
-import os.path
+from os.path import abspath, join
+from os.path import dirname as d
+
 
 import pytest
 from boj.core import util
@@ -7,16 +9,20 @@ from boj.core.error import ParsingConfigError
 
 
 @pytest.mark.parametrize(
-    "test_conf",
+    "conf_name",
     [
-        util.read_yaml(os.path.join("tests", "assets", "config", "test_filetype_config.yaml")),
+        "test_filetype_config.yaml",
     ],
 )
-def test_filetype_config(test_conf):
+def test_filetype_config(request, conf_name):
+    rootdir = request.config.rootdir
     conf = Config.load(
         suffix="test_filetype_config.yaml",
-        cwd=os.path.join(os.getcwd(), "tests", "assets", "config"),
+        cwd=join(rootdir, "tests", "assets", "config"),
     )
+
+    test_conf = util.read_yaml(join(rootdir, "tests", "assets", "config", conf_name))
+
     filetype_conf = conf.of_filetype("cpp")
     assert filetype_conf.language == test_conf["filetype"]["cpp"]["language"]
     assert filetype_conf.compile == test_conf["filetype"]["cpp"]["compile"]
@@ -24,24 +30,31 @@ def test_filetype_config(test_conf):
 
 
 @pytest.mark.parametrize(
-    ("suffix", "test_conf"),
+    ("conf_name"),
     [
-        (
-            "test_filetype_no_lang_config.yaml",
-            util.read_yaml(os.path.join("tests", "assets", "config", "test_filetype_no_lang_config.yaml")),
-        ),
-        (
-            "test_filetype_no_run_config.yaml",
-            util.read_yaml(os.path.join("tests", "assets", "config", "test_filetype_no_run_config.yaml")),
-        ),
+        "test_filetype_no_lang_config.yaml",
+        "test_filetype_no_run_config.yaml",
     ],
 )
-def test_filetype_config_throws_error(suffix, test_conf):
+def test_filetype_config_throws_error(request, conf_name):
+    rootdir = request.config.rootdir
     conf = Config.load(
-        suffix=suffix,
-        cwd=os.path.join(os.getcwd(), "tests", "assets", "config"),
+        suffix=conf_name,
+        cwd=join(rootdir, "tests", "assets", "config"),
     )
+
     with pytest.raises(ParsingConfigError) as e:
         conf.of_filetype("cpp")
 
     assert e.type == ParsingConfigError
+
+
+def test_ini(request):
+    print(request.config.rootdir)
+    print(request.config.rootdir)
+    print(request.config.rootdir)
+    print(request.config.rootdir)
+    print(request.config.rootdir)
+    print(request.config.rootdir)
+    print(request.config.rootdir)
+    assert True
