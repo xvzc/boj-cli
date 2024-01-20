cfd5de3d5567bdb609f3f9335545f806b84d1ba9
xvzc <dev.kwanghoo@gmail.com>
2024-01-20 17:43:20 +0900

    feat: allow boj open command run without id We can get the problem id from .boj-info.json in case that we run `boj open` in the problem directory.

--- boj/commands/open/open_command.py
+++ boj/commands/open/open_command.py
@@ -2,12 +2,24 @@ import webbrowser
 
 import boj.core.constant
 from boj.core.base import Command
-from boj.core.config import Config
 from boj.core.out import BojConsole
+from boj.core.config import Config
+from boj.data.boj_info import BojInfo
 
 
 class OpenCommand(Command):
     def execute(self, args):
         console = BojConsole()
         with console.status("Opening in browser..."):
-            webbrowser.open(boj.core.constant.boj_problem_url(args.problem_id))
+            problem_id = None
+            if args.problem_id:
+                problem_id = args.problem_id
+            else:
+                config = Config.load()
+                boj_info = BojInfo.find_any(
+                    problem_dir=config.workspace.problem_dir,
+                    problem_id=None
+                )
+                problem_id = boj_info.id
+
+            webbrowser.open(boj.core.constant.boj_problem_url(problem_id))
